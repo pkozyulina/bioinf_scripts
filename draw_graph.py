670538e26c7128a8c1bde2bf9e55a9c63d615ec9
pkozyulina <polykoz@gmail.com>
2017-03-10 14:55:32 +0300

    Update draw_graph.py

--- draw_graph.py
+++ draw_graph.py
@@ -14,13 +14,37 @@ def twin(read):
     return Seq.reverse_complement(read)
 
 # Check input file format
-def filetype(file):
-    return str(file.readline).strip().startswith(">")
+def is_fasta(file):
+    return str(file.readline()).strip().startswith(">")
 
 # Splits sequence to kmers
-def split_read_to_kmer(seq,k):
+def split_read_fasta(file):
+    read = ""
+    cnt = 0
+    for line in file:
+        current_line = str(line).strip()
+        if not current_line.startswith(">"):
+            read += current_line
+            continue
+        current_line = ""
+        if read != "":
+            yield read
+            read = ""
+    yield read
+
+def split_read(seq, k):
     for i in range(len(seq)-k+1):
         yield seq[i:i+k]
+
+# Splits sequence to kmers
+def split_read_fastq(file):
+    for line in file:
+        _ = line
+        read = next(file).strip()
+        next(file)
+        next(file)
+        yield read
+
 
 # Generates next or previous step of a graph possibilities
 def fw(km):
@@ -34,24 +58,19 @@ def bw(km):
 # Parses all reads in input file file and creates a dict with kmers
 def fileparse(file, k=55, limit=1):
     d = collections.defaultdict(int)
-    if filetype(file):
-        for line in file:
-            read = line.strip()
-            for km in split_read_to_kmer(read, k):
+    if is_fasta(file):
+        for read in split_read_fasta(file):
+            for km in split_read(read, k):
                 d[km] += 1
             read = twin(read)
-            for km in split_read_to_kmer(read, k):
+            for km in split_read(read, k):
                 d[km] += 1
     else:
-        for line in file:
-            _ = line
-            read = next(file).strip()
-            next(file)
-            next(file)
-            for km in split_read_to_kmer(read, k):
+        for read in split_read_fastq(file):
+            for km in split_read(read, k):
                 d[km] += 1
             read = twin(read)
-            for km in split_read_to_kmer(read, k):
+            for km in split_read(read, k):
                 d[km] += 1
 
     return d
@@ -82,18 +101,22 @@ def combine_fw_and_bw(graph, start_km):
     contig_fw = get_contig(graph, start_km)
     contig_bw = get_contig(graph, twin(start_km))
     contig = [twin(x) for x in contig_bw[-1:0:-1]] + contig_fw
-    return contig_to_string(contig) #, contig
+    coverage = 0
+    for con in contig:
+        if con in graph:
+            coverage += graph[con]
+    return contig_to_string(contig), coverage
 
 # Finds all possible contigs
 def find_all_contigs(graph):
-    contigs = set()
+    contigs = collections.defaultdict(int)
     checked = set()
     for km in graph:
         if km not in checked:
             checked.add(km)
-            current_con = combine_fw_and_bw(graph, km)
-            contigs.add(current_con)
-            contigs.add(twin(current_con))
+            current_con, coverage = combine_fw_and_bw(graph, km)
+            contigs[current_con] = coverage
+            contigs[twin(current_con)] = coverage
         else:
             pass
     return contigs
@@ -105,8 +128,12 @@ def draw_dot(contigs, output_file, k):
         for contig in contigs:
             start = contig[0:(k-1)]
             end = contig[(len(contig) - (k-1)):len(contig)]
-            L = len(contig[k:(len(contig) - (k-1))])
-            outp.write('%s -> %s[label="C L %i"]\n' % (start, end, L))
+            length_c = len(contig[k:(len(contig)-1)])
+            if length_c != 0:
+                coverage = contigs[contig]/length_c
+            else:
+                coverage = contigs[contig]
+            outp.write('%s -> %s[label="C %i L %i"]\n' % (start, end, coverage, length_c))
         outp.write("}")
 
 
@@ -122,4 +149,5 @@ def main(input_file, output_file, k):
 ######## SCRIPT BODY #################
 if __name__ == "__main__":
     main(args.input, args.output, args.kmer)
+
 
